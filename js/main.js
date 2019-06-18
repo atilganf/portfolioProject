486f1a9f6fa289f7507b62e18430faade7478b77
atilganf <atilganyf@gmail.com>
2019-06-18 04:53:06 +0300

    main page completed

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 function displaynav(){
     var menu = document.querySelector("nav");
     if(menu.style.height == "0px"){
-        menu.style.height = "340px";
+        menu.style.height = "18.31em";
     }else{
         menu.style.height = "0px";
     }
