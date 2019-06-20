4fc02880389d3c654583ebd584c174520fefa5eb
atilganf <atilganyf@gmail.com>
2019-06-20 03:49:20 +0300

    second page completed

--- js/main.js
+++ js/main.js
@@ -1,9 +1,15 @@
-function displaynav(){
+function displaynav() {
     var menu = document.querySelector("nav");
-    if(menu.style.height == "0px"){
+    if (menu.style.height == "0px") {
         menu.style.height = "18.31em";
-    }else{
+    } else {
         menu.style.height = "0px";
     }
-
+}
+function animateBars() {
+    let widths = [80, 80, 70, 50];
+    var skills = document.getElementsByClassName("skill");
+    for (let i = 0; i < skills.length; i++) {
+        skills[i].style.width = widths[i] + "%";
+    }
 }
